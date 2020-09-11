87c4d32b46d0fa087f28380c883b38e8c5d04506
Debarshi <debopiku1122@gmail.com>
2020-09-11 10:22:16 +0530

    Rate Limiter

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+
 app.use(cookieParser());
 app.use(express.json());
-
 
 const Tour_Router = require('./Routes/Tour_Routes');
 const User_Router = require('./Routes/User_Routes');
 
-
 const AppError = require('./Classes/appError.js');
 const { errorfunction } = require('./Controllers/errorHandleController.js');
 
+const limiter = rateLimit({
+      max: 10,
+      windowMs: 60 * 60 * 1000,
+      message: 'Too many Request from this IP! Please try again in an hour!'
+});
+app.use('/api', limiter);
 
 // Routes Mounting .................................
 app.use('/api/v1/tours', Tour_Router);
