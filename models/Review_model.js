6f2dd2bb9f9599dba414bdf01950e76c27aa606c
Debarshi <debopiku1122@gmail.com>
2020-09-13 21:05:56 +0530

    creating review  model

---
+++ models/Review_model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+        required: ['true', 'Message cannot be blank!'],
+        maxlength: 255
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    forTour: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour'
+        }
+    ],
+    whichUser: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
