9d1901995467eebbad456d6e99adc33b6092b214
Debarshi <debopiku1122@gmail.com>
2020-09-14 21:49:03 +0530

    get specefic based on tour

--- Controllers/ReviewController.js
+++ Controllers/ReviewController.js
@@ -5,7 +5,7 @@ exports.create_review = async (req, res, next) => {
     try {
         if (!req.body.forTour) req.body.forTour = req.params.tourId;
         if (!req.body.whichUser) req.body.whichUser = req.user.id;
-        
+
         const createNew = await Review.create(req.body);
         res.status(200).json({
             status: 'Success',
@@ -18,7 +18,9 @@ exports.create_review = async (req, res, next) => {
 
 exports.get_all_review = async (req, res, next) => {
     try {
-        const createNew = await Review.find();
+        let filter = {};
+        if (req.params.tourId) filter = { forTour: req.params.tourId };
+        const createNew = await Review.find(filter);
         res.status(200).json({
             status: 'Success',
             result: createNew
