b00a8c107ece10eea33801754059718ee47eb707
Debarshi <debopiku1122@gmail.com>
2020-09-21 19:07:49 +0530

    fetching in ovevriwe donw

--- Controllers/ViewController.js
+++ Controllers/ViewController.js
@@ -1,8 +1,16 @@
+const Tour = require('../models/Tour_models');
+const AppError = require('../Classes/appError');
 
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
+exports.getOverview = async (req, res, next) => {
+    try {
+        const tour = await Tour.find();
+        res.status(200).render('overview', {
+            title: 'All Tours',
+            result: tour
+        });
+    } catch (error) {
+        return next(new AppError(`${error}`));
+    }
 }
 
 exports.getTours = (req, res) => {
