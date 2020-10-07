00bb4e74f770844e36a3693499851652d02fb099
Debarshi <debopiku1122@gmail.com>
2020-10-07 23:01:51 +0530

    booking model

---
+++ models/Booking_model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Tour = require('./Tour_models');
+
+const bookingSchema = new mongoose.Schema({
+  bookingtour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must have a tour']
+  },
+  bookingUser: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a Price']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  paid: {
+    type: Boolean,
+    default: true
+  }
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('bookingUser').populate({
+    path: 'bookingtour',
+    select: 'name'
+  });
+});
+
+
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
