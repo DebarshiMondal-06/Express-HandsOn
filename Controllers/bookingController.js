3b3f9e090d8656744aefb0726b3041a79991332c
Debarshi <debopiku1122@gmail.com>
2020-10-08 08:12:25 +0530

    redirect to overview after booking querystring

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/Tour_models');
 const AppError = require('../Classes/appError');
+const bookingModel = require('../models/Booking_model');
 
 exports.getCheckout = async (req, res, next) => {
   try {
@@ -8,8 +9,7 @@ exports.getCheckout = async (req, res, next) => {
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourID}
-      &user=${req.user.id}&price=${tour.price}`,
+      success_url: `${req.protocol}://${req.get('host')}?tour=${req.params.tourID}&user=${req.user.id}&price=${tour.price}`,
 
       cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
       customer_email: req.user.email,
@@ -24,7 +24,6 @@ exports.getCheckout = async (req, res, next) => {
         }
       ]
     });
-
     res.status(200).json({
       status: 'success',
       session
@@ -34,11 +33,20 @@ exports.getCheckout = async (req, res, next) => {
   }
 }
 
-exports.createBookingCheckout = (req, res, next) => {
-  const { tour, user, price } = req.query;
-  if (!tour && !user && !price) return next();
-
-
+exports.createBookingCheckout = async (req, res, next) => {
+  try {
+    const { tour, user, price } = req.query;
+    if (!tour && !user && !price) return next();
+    await bookingModel.create({
+      bookingtour: tour,
+      bookingUser: user,
+      price
+    });
+    res.redirect(req.originalUrl.split('?')[0]);
+    next();
+  } catch (error) {
+    return next(new AppError(`${error}`, 500));
+  }
 }
 
 
--- Routes/View_Routes.js
+++ Routes/View_Routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const View_Router = express.Router();
 const View_Controller = require('../Controllers/ViewController');
 const authController = require('../Controllers/authController');
+const bookingController = require('../Controllers/bookingController');
 
 View_Router.get('/me', authController.protect, View_Controller.getAccount);
 
 
 View_Router.use(authController.isLoggedIn);
 
-View_Router.get('/', View_Controller.getOverview);
+View_Router.get('/', bookingController.createBookingCheckout, View_Controller.getOverview);
 View_Router.get('/tours/:id', View_Controller.getTours);
 View_Router.get('/login', View_Controller.loginpage);
 View_Router.get('/logout', authController.logout, View_Controller.logoutpage);
